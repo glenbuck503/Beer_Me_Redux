20a327cfb325d65b9822dcf637f1dfd8f1fb5b3d
glenbuck503 <glenbuck503@gmail.com>
2021-07-09 08:50:13 -0700

    bad fail for add tap test

--- src/_tests_/reducers/tap-list-reducer.test.js
+++ src/_tests_/reducers/tap-list-reducer.test.js
@@ -1,7 +1,36 @@
 import tapListReducer from '../../reducers/tap-list-reducer';
 
 describe('tapListReducer', () => {
+  let action;
+  const tapData = {
+    names: 'corona',
+    brand: 'extra',
+    alcohol: '3',
+    id: 1
+  };
+  
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(tapListReducer({}, { type: null })).toEqual({});
+  });
+
+
+  test('Should successfully add new tap data to masterTapList', () => {
+    const { names, brand, alcohol, id } = tapData;
+    action = {
+      type: 'ADD_TAP',
+      names: names,
+      brand: brand,
+      alcohol: alcohol,
+      id: id
+    };
+
+    expect(tapListReducer({}, action)).toEqual({
+      [id] : {
+        names: names,
+        brand: brand,
+        alcohol: alcohol,
+        id: id
+      }
+    });
   });
 });
