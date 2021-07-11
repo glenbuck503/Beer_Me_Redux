e6a98687b0a408266647db55bb063658f53cb85a
glenbuck503 <glenbuck503@gmail.com>
2021-07-11 14:27:45 -0700

    add combined reducers tests

--- src/_tests_/reducers/index-reducer.test.js
+++ src/_tests_/reducers/index-reducer.test.js
@@ -23,4 +23,24 @@ describe("rootReducer", () => {
   test('Check that initial state of formVisibleReducer matches root reducer', () => {
     expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, { type: null }));
   });
+
+  test('Check that ADD_TAP action works for tapListReducer and root reducer', () => {
+    const action = {
+      type: 'ADD_TAP',
+      names: 'test',
+      brand: 'tester1',
+      alcohol: 'testerAlcohol',
+      id: 1
+    }
+    store.dispatch(action);
+    expect(store.getState().masterTapList).toEqual(tapListReducer(undefined, action));
+  });
+  
+  test('Check that TOGGLE_FORM action works for formVisibleReducer and root reducer', () => {
+    const action = {
+      type: 'TOGGLE_FORM'
+    }
+    store.dispatch(action);
+    expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, action));
+  });
 });
