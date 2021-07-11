1f169be23ad56dca440c292177bb8bd2d9b486b8
glenbuck503 <glenbuck503@gmail.com>
2021-07-11 00:40:30 -0700

    bad test for adding tap. second test

--- src/_tests_/reducers/tap-list-reducer.test.js
+++ src/_tests_/reducers/tap-list-reducer.test.js
@@ -2,21 +2,16 @@ import tapListReducer from '../../reducers/tap-list-reducer';
 
 describe('tapListReducer', () => {
   let action;
-  const currentState = {
-    1: {names: 'corona',
-    brand: 'extra1',
-    alcohol: '2',
-    id: 1 },
-    2: {names: 'corona',
-    brand: 'extra2',
-    alcohol: '3',
-    id: 2 }
-  }
+  const tapData = {
+    names: 'extra',
+    brand: 'corona',
+    alochol: 'test alcohol',
+    id: 1
+  };
   
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(tapListReducer({}, { type: null })).toEqual({});
   });
-
 
   test('Should successfully add new tap data to masterTapList', () => {
     const { names, brand, alcohol, id } = tapData;
@@ -32,22 +27,12 @@ describe('tapListReducer', () => {
       [id] : {
         names: names,
         brand: brand,
-        alcohol: alcohol,
+        alochol: alcohol,
         id: id
       }
     });
-
-    test('Should successfully delete a tap', () => {
-      action = {
-        type: 'DELETE_TAP',
-        id: 1
-      };
-      expect(tapListReducer(currentState, action)).toEqual({
-        2: {names: 'corona',
-          brand: 'extra2',
-          alcohol: '3',
-          id: 2 }
-      });
-    });
   });
-});
+
+
+
+  });
--- src/reducers/tap-list-reducer.js
+++ src/reducers/tap-list-reducer.js
@@ -1,16 +1,3 @@
 export default (state = {}, action) => {
-  const { names, brand, alcohol, id } = action;
-  switch (action.type) {
-  case 'ADD_TAP':
-    return Object.assign({}, state, {
-      [id]: {
-        names: names,
-        brand: brand,
-        alcohol: alcohol,
-        id: id
-      }
-    });
-  default:
-    return state;
-  }
+  return state;
 };
