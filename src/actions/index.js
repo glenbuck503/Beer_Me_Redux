50e9c49fd07f78a3fb1fd56e32f64fd45ad6f341
glenbuck503 <glenbuck503@gmail.com>
2021-07-11 14:43:18 -0700

    good test for add tap action test

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,3 +6,14 @@ export const deleteTap = id => ({
 export const toggleForm = () => ({
   type: 'TOGGLE_FORM'
 });
+
+export const addTap = (tap) => {
+  const { names, brand, alcohol, id } = tap;
+  return {
+    type: 'ADD_TAP',
+    names: names,
+    brand: brand,
+    alcohol: alcohol,
+    id: id
+  }
+}
