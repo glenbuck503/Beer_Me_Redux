55034ecb3a8564f5d7c17b7630a774d8913d7fd9
glenbuck503 <glenbuck503@gmail.com>
2021-07-11 03:40:23 -0700

    add index reducer

--- src/_tests_/reducers/index-reducer.test.js
+++ src/_tests_/reducers/index-reducer.test.js
@@ -1,8 +1,13 @@
-import formVisibleReducer from '../../reducers/form-visible-reducer';
 
-describe("formVisibleReducer", () => {
+import rootReducer from '../../reducers/index';
+
+describe("rootReducer", () => {
 
   test('Should return default state if no action type is recognized', () => {
-    expect(formVisibleReducer(false, { type: null })).toEqual(false);
+    expect(rootReducer({}, { type: null })).toEqual({
+      masterTapList: {},
+      formVisibleOnPage: false
+    });
   });
+
 });
